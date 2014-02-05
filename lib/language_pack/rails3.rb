2610a5fae31b0c4345351cd8784807d0d85f8276
Wouter de Vos <wouter@springest.com>
2014-02-05 14:47:03 +0100

    Fixbugz

--- lib/language_pack/rails3.rb
+++ lib/language_pack/rails3.rb
@@ -97,6 +97,7 @@ private
 
           FileUtils.mkdir_p(assets_metadata)
           @metadata.write(assets_version_cache, assets_version, false)
+          warn "Wrote #{@metadata.read(assets_version_cache).chomp} to the Assets version cache!"
           @metadata.save
 
           run_custom_build_steps :after_assets_precompile
